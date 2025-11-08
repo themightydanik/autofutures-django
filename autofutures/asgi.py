dee595b39f1ee9506c9702851c916733a8905566
Daniel <94014685+themightydanik@users.noreply.github.com>
2025-11-08 16:01:13 +0200

    Update asgi.py

--- autofutures/asgi.py
+++ autofutures/asgi.py
@@ -1,13 +1,23 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from apps.trading.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autofutures.settings')
 
+# Инициализируем Django ASGI приложение до импорта Channels
+django_asgi_app = get_asgi_application()
+
+# Теперь импортируем Channels
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+# Попробуем импортировать routing, если есть ошибка - используем пустой список
+try:
+    from apps.trading.routing import websocket_urlpatterns
+except ImportError:
+    websocket_urlpatterns = []
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
