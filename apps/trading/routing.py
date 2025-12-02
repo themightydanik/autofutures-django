e5d80bd225ee25cef097545e539e132b4df552a9
Daniel <94014685+themightydanik@users.noreply.github.com>
2025-12-03 01:15:32 +0200

    Update routing.py

--- apps/trading/routing.py
+++ apps/trading/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumers import TradingConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/trading/(?P<user_id>[0-9a-f\-]+)/$", TradingConsumer.as_asgi()),
+    re_path(r'^ws/(?P<user_id>[^/]+)/?$', TradingConsumer.as_asgi()),
 ]
