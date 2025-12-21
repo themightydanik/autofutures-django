b2e254c08108f60f49151a32344e7b6f1b4e2102
Daniel <94014685+themightydanik@users.noreply.github.com>
2025-12-21 18:24:19 +0200

    Update settings.py

--- autofutures/settings.py
+++ autofutures/settings.py
@@ -137,8 +137,11 @@ REST_FRAMEWORK = {
 # Channels
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    }
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
 
 # Logging
